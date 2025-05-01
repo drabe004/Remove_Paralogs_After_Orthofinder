398b90fd524850a04293cf6c857114478c115f60
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-05-01 14:37:12 -0500

    Update CountDupsInAlns_andSPCount.sh

--- CountDupsInAlns_andSPCount.sh
+++ CountDupsInAlns_andSPCount.sh
@@ -5,26 +5,26 @@
 #SBATCH -c 16
 #SBATCH --mem=16g
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=drabe004@umn.edu
+#SBATCH --mail-user=your.email@institution.edu
 #SBATCH --job-name=CountDupes_andSpecs
-#SBATCH --partition=agsmall
-#SBATCH --account=mcgaughs
+#SBATCH --partition=your_partition
+#SBATCH --account=your_account_name
 
+# Navigate to the working directory
+cd /path/to/working/directory/Count_Duplications_in_Alignments
 
-cd /panfs/jay/groups/26/mcgaughs/drabe004/Orthofinder_Datasets/125_Species_OFFICIALDATASET/OrthoFinder/Results_Jun27/Count_Duplications_in_Alignments
-
-# Load the necessary Python module
+# Load the necessary module (adjust as needed)
 module load python
 
-# Activate the Python virtual environment
-source ~/envs/myenv/bin/activate
+# Activate your Python virtual environment (update with actual path or remove if not used)
+source ~/path/to/your/env/bin/activate
 
-# Define variables for input and output directories/files
-ALNDIR=/panfs/jay/groups/26/mcgaughs/drabe004/Orthofinder_Datasets/125_Species_OFFICIALDATASET/OrthoFinder/Results_Jun27/1HighQualFilterMSAs_CleanANDFiltered2x/2Alignments_withGeneSymbols/2Alignments_GeneSymbolsANDGeneIDs/2MajorityGeneID_alignments
+# Define variables for input and output
+ALNDIR=/path/to/filtered_alignments_directory
 SPECIES_LIST=Species_List.txt
 CAVEFISH_LIST=Cavefish_List.txt
 OUTPUT_FILE=CountDupesANDspecies.csv
 LOG_FILE=logfile.log
 
-# Run the Python script with the specified arguments
-python CountDupsInAlns_andSPCount.py $ALNDIR $SPECIES_LIST $CAVEFISH_LIST $OUTPUT_FILE $LOG_FILE
+# Run the Python script
+python CountDupsInAlns_andSPCount.py "$ALNDIR" "$SPECIES_LIST" "$CAVEFISH_LIST" "$OUTPUT_FILE" "$LOG_FILE"
