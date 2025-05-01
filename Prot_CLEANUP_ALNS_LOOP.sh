a6e523427e476f9ecd30740b56236f451819c13d
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-05-01 14:49:10 -0500

    Update Prot_CLEANUP_ALNS_LOOP.sh

--- Prot_CLEANUP_ALNS_LOOP.sh
+++ Prot_CLEANUP_ALNS_LOOP.sh
@@ -4,20 +4,15 @@
 #SBATCH --mem=100g
 #SBATCH --tmp=10g
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=youremail
+#SBATCH --mail-user=your.email@institution.edu
+#SBATCH --job-name=prot_cleanup
 
-
-
+# Load Python
 module load python3
 
+# Navigate to your working directory
+cd /path/to/your/project_directory
 
-cd /panfs/jay/groups/26/mcgaughs/drabe004/Orthofinder_Datasets/125_Species_OFFICIALDATASET/OrthoFinder/Results_Jun27
-
-
-#CallPython #Scripts  #InputDirectoryWithProteinAlignments #OutputDirectory #ThresholdPercentagePresenceToFirstAminoAcid #SecondaryThresholdifNoAminoAcidMeetsTheFistThreshold #GapPercentageToCutSequences(inverse)
-
+# Run protein alignment cleanup
+# Usage: script.py <input_dir> <output_dir> <start_threshold> <gap_threshold> <gap_cutoff> <log_file>
 python Prot_CLEANUP_ALNS_LOOP.py InputDirectory OutputDirectory .9 .5 .75 debug_log.txt
-
-
-
-
